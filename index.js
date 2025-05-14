f8d09d37d952624425a9e3f673a9159cb0507d79
Musfiqur Rahman <musfiqurrhaman6@gmail.com>
2025-05-14 18:46:24 +0600

    connected mongodb to the server

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const port = 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nliquld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // initailize the app with express
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// initializing mongobd client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+};
+
+run().catch(console.dir());
 
 app.get("/", (req, res) => {
   res.send("Hellow world");
