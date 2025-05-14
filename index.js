62d79872ca0fd0777095a60a264e82772e9d55fc
Musfiqur Rahman <musfiqurrhaman6@gmail.com>
2025-05-14 18:20:49 +0600

    initailize the server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const port = 3000;
+
+// initailize the app with express
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hellow world");
+});
+
+app.listen(port, () => {
+  console.log("my coffee server is runing on port ", port);
+});
