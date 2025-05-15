098250a32a8bc8a8418baf91eb05a256a7fd27a7
Musfiqur Rahman <musfiqurrhaman6@gmail.com>
2025-05-15 08:42:34 +0600

    created dynamic routes api to see one single coffee details

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ const run = async () => {
       res.send(result);
     });
 
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+
     app.delete("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
